182453c864b124a6962eb54d7fefbd0db25ed3d3
FedeManzano <154226686+FedeManzano@users.noreply.github.com>
2025-09-26 00:33:32 -0300

    fix: Inclusión de los indices de la tabla alquiler

--- tb/tb_alquiler.sql
+++ tb/tb_alquiler.sql
@@ -78,14 +78,13 @@ BEGIN
             END = 1
         ),
     );
+    -- Índices para optimizar consultas frecuentes
+    -- CREATE INDEX IX_Fecha_Alquiler ON [db_alquileres_vehiculos].[negocio].[Alquiler](FAlq)
+
+    --  Índice para consultas por estado
+    -- CREATE INDEX IX_Estado_Alquiler ON [db_alquileres_vehiculos].[negocio].[Alquiler](Estado)
 END
 ELSE PRINT('La tabla [db_alquileres_vehiculos].[negocio].[Alquiler] Ya existe en la BD: db_alquileres_vehiculos')
 
 
-
 /* DROP TABLE  [db_alquileres_vehiculos].[negocio].[Alquiler] */
--- Índices para optimizar consultas frecuentes
--- CREATE INDEX IX_Fecha_Alquiler ON [db_alquileres_vehiculos].[negocio].[Alquiler](FAlq)
-
---  Índice para consultas por estado
--- CREATE INDEX IX_Estado_Alquiler ON [db_alquileres_vehiculos].[negocio].[Alquiler](Estado)
