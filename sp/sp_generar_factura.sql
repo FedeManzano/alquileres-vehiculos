648b144b8e4a3e3ab0acfc809947db1aa7af0740
FedeManzano <154226686+FedeManzano@users.noreply.github.com>
2025-09-26 16:04:21 -0300

    fix: generar factura, calcula varios alquleres diferentes el mismo día, asignando un nro de factura diferente para cada uno de ellos

--- sp/sp_generar_factura.sql
+++ sp/sp_generar_factura.sql
@@ -4,7 +4,6 @@ GO
 CREATE OR ALTER PROCEDURE [negocio].[sp_Generar_Factura]
 @TIPO_DOC       TINYINT,
 @NRO_DOC        VARCHAR(8),
-@ID_T_V         SMALLINT,
 @F_ALQ          DATE,
 @RES            INT     OUTPUT
 AS 
@@ -13,27 +12,46 @@ BEGIN
     BEGIN TRANSACTION T_GENERAR_FACTURA
 
     BEGIN TRY
-        DECLARE @MONTO_TOTAL DECIMAL(10,2) = 
+
+        IF   
         (
-            SELECT COUNT(*) * 
-            (
-                SELECT Precio 
-                FROM [db_alquileres_vehiculos].[negocio].[Tipo_Vehiculo] 
-                WHERE ID_Tipo_Vehiculo = @ID_T_V
-            )
+            SELECT COUNT(*)
             FROM [db_alquileres_vehiculos].[negocio].[Alquiler]
             WHERE   TipoDoc             = @TIPO_DOC   AND 
                     NroDoc              = @NRO_DOC    AND 
                     FAlq                = @F_ALQ      AND 
-                    Estado              <> 1
+                    CodFactura   IS NULL 
+        ) = 0
+        BEGIN 
+            SET @RES = 0
+            RAISERROR('El alquiler ya dispone de factura', 16, 1)
+        END
+            
+        DECLARE @MONTO_TOTAL DECIMAL(10,2) = 
+        (
+            SELECT SUM(TI.Precio)
+            FROM        [db_alquileres_vehiculos].[negocio].[Alquiler]             ALQ
+            INNER JOIN  [db_alquileres_vehiculos].[negocio].[Tipo_Vehiculo]        TI 
+            ON TI.ID_Tipo_Vehiculo = ALQ.ID_T_Vehiculo 
+                WHERE   TipoDoc             =       @TIPO_DOC   AND 
+                        NroDoc              =       @NRO_DOC    AND 
+                        FAlq                =       @F_ALQ      AND 
+                        Estado              <>      1
         )
 
         IF @MONTO_TOTAL IS NULL OR @MONTO_TOTAL = 0
-            RAISERROR('El monto para la fecha solicitada es erroneo', 11, 1)
-
+        BEGIN 
+            SET @RES = 2
+            RAISERROR('El monto para la fecha solicitada es erroneo', 16, 1)
+        END
+            
         DECLARE @COD_FAC CHAR(10) = ''
-        EXEC [db_utils].[library].[sp_Str_Number_Random] 0, 9, 9, @COD_FAC OUTPUT
-        SET @COD_FAC = 'F' + @COD_FAC 
+        DECLARE @COD_FAC_AUX CHAR(6) = RIGHT
+        ( 
+            CAST(CHECKSUM(GETDATE()) AS VARCHAR(15)), 6
+        )
+
+        SET @COD_FAC = 'F' + @COD_FAC_AUX + RIGHT(@NRO_DOC, 3)
 
         WHILE EXISTS 
         (
@@ -42,8 +60,12 @@ BEGIN
             WHERE CodFactura = @COD_FAC
         )
         BEGIN 
-            EXEC [db_utils].[library].[sp_Str_Number_Random] 0, 9, 9, @COD_FAC OUTPUT
-            SET @COD_FAC = 'F' + @COD_FAC 
+            SET @COD_FAC_AUX = RIGHT
+            ( 
+                CAST(CHECKSUM(GETDATE()) AS VARCHAR(15)), 6
+            )
+            SET @COD_FAC = ''
+            SET @COD_FAC = 'F' + @COD_FAC_AUX + RIGHT(@NRO_DOC, 3)
         END
 
         INSERT INTO [db_alquileres_vehiculos].[negocio].[Factura]
@@ -52,13 +74,26 @@ BEGIN
 
         UPDATE [db_alquileres_vehiculos].[negocio].[Alquiler]
         SET     CodFactura = @COD_FAC
-        WHERE       TipoDoc             = @TIPO_DOC   AND 
-                    NroDoc              = @NRO_DOC    AND 
-                    FAlq                = @F_ALQ      AND 
-                    Estado              = 0
+        WHERE       TipoDoc             =       @TIPO_DOC   AND 
+                    NroDoc              =       @NRO_DOC    AND 
+                    FAlq                =       @F_ALQ      AND 
+                    Estado              =       0           AND 
+                    CodFactura  IS      NULL
+
+        SET @RES = 1
         COMMIT TRANSACTION T_GENERAR_FACTURA
     END TRY 
     BEGIN CATCH
+        DECLARE @MJE_ERROR  NVARCHAR(100),
+                @EST        INT,
+                @SEV        INT 
+
+        SELECT  @MJE_ERROR  = ERROR_MESSAGE(),
+                @EST        = ERROR_STATE(),
+                @SEV        = ERROR_SEVERITY()
+
+        RAISERROR(@MJE_ERROR, @SEV, @EST)        
+
         ROLLBACK TRANSACTION T_GENERAR_FACTURA
     END CATCH
 END
